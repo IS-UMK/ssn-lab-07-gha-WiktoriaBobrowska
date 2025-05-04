8f8dab7fa65cabdb9fc6ffe599020f85b7296a84
Wiktoria <wbobrowska21@gmail.com>
2025-05-04 20:49:09 +0200

    zadanie domowe

--- gha.py
+++ gha.py
@@ -1,22 +1,29 @@
 import numpy as np
+from sklearn.datasets import fetch_openml
+import matplotlib.pyplot as plt
+import random
 
 class GHA(object):
-    
     def __init__(self, n_components=1, eta=0.001, n_epochs=100):
         self.eta = eta
         self.n_components = n_components
         self.n_epochs = n_epochs
-        
+
     def init(self, X):
-        # TODO inicjuj macierz wag self.W o kształcie [n_components, ilość zmiennych ] 
-        
+        n_features = X.shape[1]
+        self.W = np.random.randn(self.n_components, n_features) * 0.01
         return self
 
     def fit(self, X):
-        # TODO algorytm uczenia GHA
-
+        self.init(X)
+        for epoch in range(self.n_epochs):
+            for x in X:
+                x = x.reshape(-1, 1)
+                y = self.W @ x
+                delta_W = self.eta * ((y @ x.T) - np.tril(y @ y.T) @ self.W)
+                self.W += delta_W
         return self
-            
+
     def transform(self, X):
         return (self.W @ X.T).T
 
